6eee5ff524c8319a24de11fd68fc3483b3343abc
František Bráblík <frantisek.brablik@gmail.com>
2022-05-21 05:11:05 +0200

    Add week

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 def str_to_days(s: str) -> int:
     if s == 'day':   return 1
+    if s == 'week':  return 7
     if s == 'month': return 30
     if s == 'year':  return 365
     raise ValueError('invalid time period')
